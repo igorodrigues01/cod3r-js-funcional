058954a7e4d23efd23a04bebb3cdaa60ba19e15c
Igor Rodrigues <igorodrigues_@hotmail.com>
2020-08-16 17:14:16 -0300

    Reacrindo map com function e prototype

--- funcoes/map.js
+++ funcoes/map.js
@@ -7,12 +7,35 @@ const cartSession = [
 
 // ******************* Map Original
 const getName = (product) => product.name;
-console.log(cartSession.map(getName));
+// console.log(cartSession.map(getName));
 
 const getSubTotal = (product) => {
   const coin = product.quantity * product.price;
   return coin.toLocaleString("pt-BR", { style: "currency", currency: "BRL" });
 };
-console.log(cartSession.map(getSubTotal));
+// console.log(cartSession.map(getSubTotal));
 
-// ********************** Recriando Map
+// ********************** Recriando Map v1
+function myMap(arr, fn) {
+  const newArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    newArray.push(fn(arr[i], i, arr));
+  }
+  return newArray;
+}
+
+// console.log(myMap(cartSession, getName));
+// console.log(myMap(cartSession, getSubTotal));
+
+// ********************** Recriando Map v2
+
+Array.prototype.myMap = function (fn) {
+  const newArray = [];
+  for (let i = 0; i < this.length; i++) {
+    newArray.push(fn(this[i], i, this));
+  }
+  return newArray;
+};
+
+console.log(cartSession.myMap(getName));
+console.log(cartSession.myMap(getSubTotal));
